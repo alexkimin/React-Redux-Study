b682b0ac9866ae02213d3a5fa075c7a81c3eec37
AlexMin314 <m98stone@gmail.com>
2018-01-12 16:21:09 +0800

    readme update

--- README.md
+++ README.md
@@ -2,4 +2,19 @@
 This toy project is for exploring interesting paradigms and modules - Functional Programming React with Redux.
 
 ## What I explored and used here
-#
+* basics of functional approach: compose, pipe, curry, stateless compoents only.
+* react router v4 (params, query with query-string)
+* async action with redux-thunk
+* duck pattern with redux-actions
+* selector pattern with reselect
+* promise action handling with
+* styled-component, scss
+* normalizr
+* applying hot loader after eject
+* meta tag handling with react-helmet
+
+# Planning
+* immutable.js
+* redux-saga
+* Storybook
+* Typescript or flow
--- src/libs/selectors/filterSelector.js
+++ src/libs/selectors/filterSelector.js
@@ -4,9 +4,12 @@ import { pipe } from 'libs'
 const _filterType = type => filter => filter === type
 const _typeChecker = _filterType('completed')
 
-const filterTodo = isfiltering => arr => isfiltering
-  ? arr.filter(e => _typeChecker('completed') === e.isCompleted)
-  : arr
+const filterTodo = (todos, isfiltering) => {
+  console.log('todo filtering')
+  return isfiltering
+  ? todos.filter(e => _typeChecker('completed') === e.isCompleted)
+  : todos
+}
 
 /*
 Reselect is a library for building memoized selectors.
@@ -15,11 +18,14 @@ snippets of the Redux state for our React components.
 Using memoization, we can prevent unnecessary rerenders and
 recalculations of derived data which in turn will speed up our application.
 */
-const getTodos = (state, filter) => state.todos
-
-export const getFilteredTodo = (state, filter) => {
-  return createSelector(
-    getTodos,
-    filterTodo(filter)
-  )(todos)
+const getTodos = (state, props) => {
+  console.log('todo input selector')
+  return state.Todo.todos
 }
+
+const getFilter = (state, props) => props.match.params.filter
+
+export const getFilteredTodo = createSelector(
+    [getTodos, getFilter],
+    filterTodo
+  )
