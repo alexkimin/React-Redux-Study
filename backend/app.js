b75045819af4611f0c890c64dc80360ab718b91a
AlexMin314 <m98stone@gmail.com>
2018-01-17 16:45:32 +0800

    heroku setting

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({
 
 app.use(cookieParser())
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, '../build')))
 
 // custom middleware
 app.use(function (req, res, next) {
